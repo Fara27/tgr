539c2a90bbc8512fe8f11b68e1309281ce328430
Fara27 <fara27days@yahoo.com>
2021-12-29 21:31:35 +0100

    composer ready

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2d9ba63aa2efcf158e2c90f2296d33fc387dbe0d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2d9ba63aa2efcf158e2c90f2296d33fc387dbe0d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
